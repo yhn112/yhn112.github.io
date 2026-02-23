cb4d792b5fa8d7aff4af9957e82db2fbdb34569c
yhn112 <yhn112@users.noreply.github.com>
2026-02-23 15:24:55 +0000

    Deploying to gh-pages from @ yhn112/yhn112.github.io@eec46f2f5c2bc25307c47c2c8f105dd12a6f013e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yhn112/diskin.me",
+    "data-repo": "yhn112/yhn112.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
