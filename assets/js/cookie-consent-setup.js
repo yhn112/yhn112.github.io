5396c11d6bbf11eea2debf66b381a8d55be45681
yhn112 <yhn112@users.noreply.github.com>
2026-04-23 13:48:54 +0000

    Deploying to gh-pages from @ yhn112/yhn112.github.io@ad6f42b29fc393d5b52caab21524e29953655a98 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://diskin.me/#contact">contact us</a>.'
+                description: 'See our <a class="cc-link" href="/privacy/">privacy policy</a> for details on cookies, analytics, and contact information.'
               }
             ]
           }
@@ -154,4 +154,3 @@ function initializeCookieConsent() {
 
 // Initialize when the library is available
 initializeCookieConsent();
-
